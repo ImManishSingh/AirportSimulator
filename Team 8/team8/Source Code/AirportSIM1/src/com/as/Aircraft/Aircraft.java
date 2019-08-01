3b52d9bf471833876b7cd7ae105b2b9621eb8bd0
Utkarsh Saraswat <saraswatisdaniel@gmail.com>
2019-08-01 13:21:07 +0530

    implemented following changes
    created state object currentstate in class aircraft
    created function allotstate in class aircraft
    created constructor in class aircraft

--- Team 8/team8/Source Code/AirportSIM1/src/com/as/Aircraft/Aircraft.java
+++ Team 8/team8/Source Code/AirportSIM1/src/com/as/Aircraft/Aircraft.java
@@ -1,14 +1,25 @@
 package com.as.Aircraft;
 
-import com.as.Aircraft.AircraftType;
+import com.as.Airport.*;
 
 public class Aircraft {
 	public int AircraftNumber; //This variable denotes the number assigned ton each aircraft.
 	public AircraftType at=new AircraftType();
-	public int bWaitTime,sWaitTime,eWaitTime,efwt=0,sfwt=0,gateNo;
+	public int bWaitTime,sWaitTime,eWaitTime, efwt=0 ,sfwt=0,gateNo;
 	public int astatetimer=0,cstatetimer=0,dstatetimer=0,rstatetimer=0,rdstatetimer=0;
-	public String state;
+	//public String state;
+
+	private State currentPosition;
 	public int RunwayNumber;
 	public static int freedtime=0;
 
+	public Aircraft(AircraftType State, int initPosition) {
+		return at;
+	}
+
+	public void allotPosition(State newposition) {
+		this.currentPosition.release();
+		this.currentPosition = newposition;
+		newposition.occupy();
+	}
 }
